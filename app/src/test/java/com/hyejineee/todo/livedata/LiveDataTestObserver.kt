a0df1087a6ae88aa080031e36395f2765dc7294f
hyejineee <dev.hyejin04@gmail.com>
2021-08-20 16:20:46 +0900

    Setup for test

---
+++ app/src/test/java/com/hyejineee/todo/livedata/LiveDataTestObserver.kt
@@ -0,0 +1,48 @@
+package com.hyejineee.todo.livedata
+
+import androidx.lifecycle.Observer
+import java.lang.AssertionError
+
+class LiveDataTestObserver<T> : Observer<T> {
+
+    private val values : MutableList<T> = mutableListOf()
+
+    override fun onChanged(t: T) {
+        values.add(t)
+    }
+
+    fun assertValueSequence(sequence:List<T>) : LiveDataTestObserver<T> {
+        var i = 0
+        val actualIterator = values.iterator()
+        val expectedIterator = sequence.iterator()
+
+        var actualNext:Boolean
+        var expectNext:Boolean
+
+        while(true){
+            actualNext = actualIterator.hasNext()
+            expectNext = expectedIterator.hasNext()
+
+            if(!actualNext || !expectNext) break
+
+            val actual :T = actualIterator.next()
+            val expected : T = expectedIterator.next()
+
+            if(actual != expected){
+                throw AssertionError("actual : ${actual}, expected : ${expected}, index:${i}")
+            }
+
+            i++
+        }
+
+        if (actualNext){
+            throw AssertionError("More valued received than expected ($i)")
+        }
+
+        if(expectNext){
+            throw AssertionError("Fewer values received than expected ($i)")
+        }
+
+        return this
+    }
+}
--- app/src/test/java/com/hyejineee/todo/viewmodel/todo/ViewModelTest.kt
+++ app/src/test/java/com/hyejineee/todo/viewmodel/todo/ViewModelTest.kt
@@ -1,8 +1,10 @@
 package com.hyejineee.todo.viewmodel.todo
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import com.hyejineee.todo.DI.appTestModule
 import com.hyejineee.todo.InstantExecutorListener
+import com.hyejineee.todo.livedata.LiveDataTestObserver
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.mockk
@@ -14,9 +16,7 @@ import kotlinx.coroutines.test.setMain
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
-import org.koin.dsl.module
 import org.koin.test.KoinTest
-import java.security.AccessControlContext
 
 @ExperimentalCoroutinesApi
 internal class ViewModelTest : DescribeSpec(), KoinTest {
@@ -40,6 +40,13 @@ internal class ViewModelTest : DescribeSpec(), KoinTest {
         stopKoin()
         Dispatchers.resetMain() // 메인 디스패처를 초기화해줘야 메모리 누수가 발생하지 않는다.
     }
+
+    protected fun <T> LiveData<T>.test(): LiveDataTestObserver<T> {
+        val testObserver = LiveDataTestObserver<T>()
+        observeForever(testObserver)
+        return testObserver
+    }
+
 
     init {
         listener(InstantExecutorListener())
